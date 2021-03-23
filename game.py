1ad8f744a239a076e223c5987e979306c59394c3
TooClose <truproximity@gmail.com>
2021-03-22 22:09:02 -0400

    day1

--- game.py
+++ game.py
@@ -3,11 +3,21 @@
 # Depression Theme
 import sys
 
+dp = True
 def needHelp():
     print("Welcome to your free therapy session! \nLet's start with some basic questions...")
     print("How many times have you felt sad today?")
     inp = input("")
     if (int(inp) > 4 ):
         print("Damn, well its gonna get better from here.")
-    
-needHelp()
+    elif (int(inp) < 4):
+        print("Are you sure you need our help? Try to also think about the times you were happy today!")
+
+def roughDay():
+    return 0
+
+def okDay():
+
+# main method loop
+while(dp):
+    needHelp()
